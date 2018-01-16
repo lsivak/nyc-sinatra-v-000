8b550bc54e847aeb907622b4654a2b8008fbca41
Lara Sanders <larasanders@gmail.com>
2018-01-16 20:21:30 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,43 +1,41 @@
 class LandmarksController < ApplicationController
 
   get '/' do
-    redirect to '/landmarks'
+    redirect '/landmarks'
   end
 
   get '/landmarks' do
-    @landmarks=Landmark.all
-    #  @figures=Figure.all
+    @landmarks = Landmark.all
+    @figures = Figure.all
     erb :'landmarks/index'
   end
 
   get '/landmarks/new' do
-    erb :'landmarks/new'
+    erb :"landmarks/new"
   end
 
-    post '/landmarks' do
-    @landmark=Landmark.find_or_create_by(params[:landmark])
-    @landmark.figure=Figure.find_or_create_by(name: params[:figure])
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
 
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  #edits landmark
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
     @landmark.save
-      redirect to "/landmarks/#{@landmark.id}"
-    end
-
-    get '/landmarks/:id' do
-      @landmark = Landmark.find(params[:id])
-      erb :'landmarks/show'
-    end
-
-    get '/landmarks/:id/edit' do
-      @landmark = Landmark.find(params[:id])
-      erb :'landmarks/edit'
-    end
-
-    patch '/landmarks/:id' do
-      @landmark =  Landmark.find_by(params[:id])
-      @landmark.name = params["landmark"]["name"]
-      @landmark.year_completed = params["landmark"]["year_completed"]
-      @landmark.save
-      redirect to "/landmarks/#{@landmark.id}"
-    end
-
+    redirect to "/landmarks/#{@landmark.id}"
   end
+
+  post "/landmarks" do
+    Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"])
+    redirect '/landmarks'
+  end
+
+end
