d41f565fae0c24d8b5312f8c57f21ade4d210eba
Lara Sanders <larasanders@gmail.com>
2018-01-16 20:55:01 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,21 +10,6 @@ class FiguresController < ApplicationController
       erb :'figures/new'
     end
 
-
-
-    post '/figures' do
-       @figure = Figure.create(params["figure"])
-       if !params[:landmark][:name].empty?
-         @figure.landmarks << Landmark.create(params[:landmark])
-       end
-
-       if !params[:title][:name].empty?
-         @figure.titles << Title.create(params[:title])
-       end
-
-       @figure.save
-       redirect to "/figures/#{@figure.id}"
-     end
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
@@ -35,10 +20,25 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
+      post '/figures' do
+         @figure = Figure.create(params[:figure])
+
+         if !params[:title][:name].empty?
+           @figure.titles << Title.create(params[:title])
+         end
+
+         if !params[:landmark][:name].empty?
+           @figure.landmarks << Landmark.create(params[:landmark])
+         end
+
+         @figure.save
+         redirect to "/figures/#{@figure.id}"
+       end
+
   post '/figures/:id' do
     @figure = Figure.find(params[:id])
-    @figure.update(params[:figure])
 
+    @figure.update(params[:figure])
     if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
