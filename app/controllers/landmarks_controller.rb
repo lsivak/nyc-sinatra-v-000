1de0dd322a431c05909fec707f4908dec8bdd1c6
Lara Sanders <larasanders@gmail.com>
2018-01-17 01:43:50 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -37,18 +37,4 @@ class LandmarksController < ApplicationController
     redirect to "/landmarks/#{@landmark.id}"
   end
 
-  #edits landmark
-  post '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    @landmark.name = params["landmark"]["name"]
-    @landmark.year_completed = params["landmark"]["year_completed"]
-    @landmark.save
-    redirect to "/landmarks/#{@landmark.id}"
-  end
-
-  post "/landmarks" do
-    Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"])
-    redirect '/landmarks'
-  end
-
 end
