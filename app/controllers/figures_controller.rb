ae0333ebf1c1f0018b51cd7e2c9d3080c70e56ea
Lara Sanders <larasanders@gmail.com>
2018-01-17 01:52:51 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,13 +2,13 @@ class FiguresController < ApplicationController
 
 
   get '/figures' do
-      @figures = Figure.all
-      erb :'figures/index'
-    end
-
-    get '/figures/new' do
-      erb :'figures/new'
-    end
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+  
+  get '/figures/new' do
+    erb :'figures/new'
+  end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
@@ -20,20 +20,20 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
-      post '/figures' do
-         @figure = Figure.create(params[:figure])
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
 
-         if !params[:title][:name].empty?
-           @figure.titles << Title.create(params[:title])
-         end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
 
-         if !params[:landmark][:name].empty?
-           @figure.landmarks << Landmark.create(params[:landmark])
-         end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
 
-         @figure.save
-         redirect to "/figures/#{@figure.id}"
-       end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 
   post '/figures/:id' do
     @figure = Figure.find(params[:id])
